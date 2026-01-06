261e34608f31473901843ff8558840b5b812da65
David Ortega Farrerons <atreidesd32@gmail.com>
2026-01-06 16:06:48 +0100

    Add test for middleware

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"snippetbox.davidortegafarrerons.com/internal/assert"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	//httptest.ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Send ResponseRecorder to keep all the Response information
+	ping(rr, r)
+
+	rs := rr.Result()
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
